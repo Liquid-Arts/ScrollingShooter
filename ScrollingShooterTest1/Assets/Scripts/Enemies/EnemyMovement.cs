c84f1b4c5d111563f56d07eed57275a5981f2d1c
haofabs <jflsalas@yahoo.de>
2015-01-20 19:41:40 +0100

    Enemy and Music Added
    Enemy with Sprite follows the player, Background Music sample

---
+++ ScrollingShooterTest1/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour 
+{
+	Transform player;
+	NavMeshAgent nav;
+	public float speed;
+	Vector3 enemyToPlayer;
+	Rigidbody2D enemyRigidBody;
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <NavMeshAgent> ();
+		enemyRigidBody = GetComponent<Rigidbody2D> ();
+		
+	}
+
+	void Update ()
+	{
+		enemyToPlayer = player.position - transform.position;
+		enemyToPlayer.z = 0f;
+		enemyToPlayer = enemyToPlayer.normalized;
+
+		enemyRigidBody.velocity = enemyToPlayer * speed;
+	}
+
+}
