498027cabb8e0c84b329aa7f777643381eabe385
haofabs <jflsalas@yahoo.de>
2015-01-27 21:50:30 +0100

    Fixed collision type on player health
    All enemy collision meshes changed to trigger

--- ScrollingShooterTest1/Assets/Scripts/Player/PlayerHealth.cs
+++ ScrollingShooterTest1/Assets/Scripts/Player/PlayerHealth.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
 
-	void OnCollisionEnter2D (Collision2D col)
+	void OnTriggerEnter2D (Collider2D col)
 	{
 		if (col.gameObject.tag == "Enemy") {
 			/*col.gameObject.GetComponent<EnemyMovement>().enabled = false;*/
