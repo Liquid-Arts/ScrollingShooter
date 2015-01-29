9086853697887a452f0d3fa2358a0037a223e06e
haofabs <jflsalas@yahoo.de>
2015-01-29 20:01:37 +0100

    Enemies and obstacle added
    Enemies follow the player using nav mesh and go around a static obstacle

--- NavMesh2D/Assets/Scripts/Enemies/EnemyNav.cs
+++ NavMesh2D/Assets/Scripts/Enemies/EnemyNav.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class EnemyNav : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	Transform player;
+	NavMeshAgent nav;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <NavMeshAgent> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+
+	void Update()
+	{
+		nav.SetDestination (player.position);
 	}
 }
