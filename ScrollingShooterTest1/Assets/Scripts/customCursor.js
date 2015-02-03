21b0b935fa8c5828000852cb8a214b63465c9f31
haofabs <jflsalas@yahoo.de>
2015-02-03 22:44:24 +0100

    In-Game Cursor changed to targeting cross
    Added script for changing the appearance of the mouse under customCursor

---
+++ ScrollingShooterTest1/Assets/Scripts/customCursor.js
@@ -0,0 +1,13 @@
+﻿var yourCursor : Texture2D;  // Your cursor texture
+var cursorSizeX : int = 16;  // Your cursor size x
+var cursorSizeY : int = 16;  // Your cursor size y
+ 
+function Start()
+{
+    Screen.showCursor = false;
+}
+ 
+function OnGUI()
+{
+    GUI.DrawTexture (Rect(Event.current.mousePosition.x-cursorSizeX/2, Event.current.mousePosition.y-cursorSizeY/2, cursorSizeX, cursorSizeY), yourCursor);
+}
