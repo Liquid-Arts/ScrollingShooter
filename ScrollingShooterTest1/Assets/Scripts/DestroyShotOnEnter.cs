c4310d772ea47d815858c1a7248cd15edef33b64
haofabs <jflsalas@yahoo.de>
2015-02-08 22:22:50 +0100

    Merged prototype with Layout
    commit 0ad3f9f2c1c5860e5414d30cdef7da7aa3a4de28
    Author: haofabs <jflsalas@yahoo.de>
    Date:   Sun Feb 8 22:18:17 2015 +0100
    
        Layout Branch is running again :D
    
    commit 93685e901ad4813e2752a50630a3758ce8acd4a7
    Author: haofabs <jflsalas@yahoo.de>
    Date:   Sun Feb 8 21:38:35 2015 +0100
    
        Layout branch

---
+++ ScrollingShooterTest1/Assets/Scripts/DestroyShotOnEnter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyShotOnEnter : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Shot") {
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ ScrollingShooterTest1/Assets/Scripts/Spawns/LevelScripts/LayoutTestLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayoutTestLevel : BaseSpawnScript  {
+
+	public GameObject basicEnemy;
+	public GameObject zigZagEnemy;
+	public GameObject boss;
+	
+	public Transform top;
+	public Transform left;
+	public Transform right;
+	public Transform bottom;
+	
+	void Start () {
+		AddFormation (new RectFormationBuilder (basicEnemy, left, 1)
+		              .WithLength (3)
+		              .WithWidth (1)
+		              .WithHorizontalSeparation (1)
+		              .WithVerticalSeparation (2));
+		AddFormation (new RectFormationBuilder (basicEnemy, right, 1)
+		              .WithLength (3)
+		              .WithWidth (1)
+		              .WithHorizontalSeparation (1)
+		              .WithVerticalSeparation (2));
+		AddFormation (new LineFormationBuilder (zigZagEnemy, bottom, 7)
+		              .WithLength (5)
+		              .WithSeparation (1.5));
+		AddSpawn (boss, top, 10);
+	}
+}
