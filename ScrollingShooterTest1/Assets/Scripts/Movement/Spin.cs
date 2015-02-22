d2b207fe4c22f508a01370b8598bd3585c23466c
Isaac Montenegro <isaacmj@yahoo.com>
2015-02-22 17:17:53 -0600

    Added targeted shots
    Created enemy that shoots proyectiles that target the current position of the player

---
+++ ScrollingShooterTest1/Assets/Scripts/Movement/Spin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+	public float speed;
+
+	private static readonly float speedMultiplier = 10;
+
+	void Update ()
+	{
+		transform.Rotate(Vector3.forward, speed * speedMultiplier * Time.deltaTime);
+	}
+}
---
+++ ScrollingShooterTest1/Assets/Scripts/Movement/TargetPlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetPlayerMovement : MonoBehaviour {
+	public float speed;
+
+	void Start () {
+		Transform player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		Vector3 enemyToPlayer = player.position - transform.position;
+		enemyToPlayer.z = 0f;
+		enemyToPlayer = enemyToPlayer.normalized;
+
+		rigidbody2D.velocity = enemyToPlayer * speed;
+	}
+}
